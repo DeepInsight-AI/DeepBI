0a7f1abf2f0e0af43dbfedcdfe7acfd04743e74f
zhuxiangchneg <1264521359@qq.com>
2023-12-01 19:32:15 +0800

    test pdf

--- client/app/pages/testdialogue/components/AutoPilot/AutoPilot.jsx
+++ client/app/pages/testdialogue/components/AutoPilot/AutoPilot.jsx
@@ -20,16 +20,30 @@ const AutoPilot = memo(({ content }) => {
   });
   }, [content]);
   const exportPdf = () => {
-   html2pdf(autopilotRef.current, {
-      filename: 'autopilot.pdf',
-      image: { type: 'jpeg', quality: 1 },
-      html2canvas: {
-        dpi: 192,
-        scale:2,
-        letterRendering: true,
-        useCORS: true
-      },
-      });
+  //  html2pdf(autopilotRef.current, {
+  //     filename: 'autopilot.pdf',
+  //     image: { type: 'jpeg', quality: 1 },
+  //     html2canvas: {
+  //       dpi: 192,
+  //       scale:2,
+  //       letterRendering: true,
+  //       useCORS: true
+  //     },
+  //     });
+      const opt = {
+        margin: 10,
+        filename: 'converted_pdf.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'mm', format: 'a2', orientation: 'portrait' } // 设置 A2 纸张大小
+      };
+  
+      // 使用 html2pdf 库生成 PDF
+      html2pdf()
+        .from(autopilotRef.current)
+        .set(opt)
+        .save();
+  
   }
   return <div className="auto_pilot" >
     <div className="auto_pilot_icon" onClick={exportPdf}>

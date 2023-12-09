502d9ad363b757748d68cfb0fa04749654642ddd
zhuxiangchneg <1264521359@qq.com>
2023-12-09 11:10:26 +0800

    fix openaikey

--- client/app/pages/settings/OpenKey.jsx
+++ client/app/pages/settings/OpenKey.jsx
@@ -22,13 +22,13 @@ const SettingsOpenKey = () => {
       if(!data.in_use){
         form.setFieldsValue(data);
       }else{
-        const {Openai, Holmes} = data;
+        const {OpenAI, DeepThought} = data;
         form.setFieldsValue({
-        ApiKey: Holmes.ApiKey,
-        OpenaiApiKey: Openai.OpenaiApiKey,
-        HttpProxyHost: Openai.HttpProxyHost,
-        HttpProxyPort: Openai.HttpProxyPort,
-        ApiHost: Openai.ApiHost
+        ApiKey: DeepThought.ApiKey,
+        OpenaiApiKey: OpenAI.OpenaiApiKey,
+        HttpProxyHost: OpenAI.HttpProxyHost,
+        HttpProxyPort: OpenAI.HttpProxyPort,
+        ApiHost: OpenAI.ApiHost
     });
       }
     createWebSocket()
@@ -41,14 +41,14 @@ const SettingsOpenKey = () => {
   const handOpenKey = ()=>{
     const data ={
       in_use: aiOption,
-      Openai: {
-        OpenaiApiKey: form.getFieldValue("OpenaiApiKey"),
-        HttpProxyHost:  form.getFieldValue("HttpProxyHost"),
-        HttpProxyPort: form.getFieldValue("HttpProxyPort"),
+      OpenAI: {
+        OpenaiApiKey: form.getFieldValue("OpenaiApiKey") || "",
+        HttpProxyHost:  form.getFieldValue("HttpProxyHost") || "",
+        HttpProxyPort: form.getFieldValue("HttpProxyPort") || "",
         ApiHost: form.getFieldValue("ApiHost") || "",
       },
-      Holmes: {
-        ApiKey: form.getFieldValue("ApiKey")
+      DeepThought: {
+        ApiKey: form.getFieldValue("ApiKey") || "",
       }
       
     }
@@ -132,18 +132,18 @@ const SettingsOpenKey = () => {
       <h4 style={{marginRight:"30px"}}>AI:</h4>
             <Radio.Group onChange={handleRadioChange} value={aiOption}>
            
-              <Radio value="Holmes">DeepThought</Radio>
-              <Radio value="Openai">OpenAI</Radio>
+              <Radio value="DeepThought">DeepThought</Radio>
+              <Radio value="OpenAI">OpenAI</Radio>
             </Radio.Group>
       </div>
           </Form.Item>
-          {aiOption === 'Holmes' && (
+          {aiOption === 'DeepThought' && (
             <Form.Item name="ApiKey" label="ApiKey" rules={[{ required: true, message: 'Please enter API key' }]}>
               <Input placeholder="ApiKey" />
             </Form.Item>
           )}
 
-{aiOption === 'Openai' && (
+{aiOption === 'OpenAI' && (
             <>
              <Form.Item name="OpenaiApiKey" label="OpenaiApiKey"  rules={[{ required: true, message: window.W_L.please_enter_api_key }]}>
         <Input placeholder="OpenaiApiKey" />

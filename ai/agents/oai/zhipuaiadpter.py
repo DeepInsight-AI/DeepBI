5ca4e1a3ddab88fdfbb762e75a470cb5291d4f9d
zhishufei <luuijie123@163.com>
2024-01-24 19:14:40 +0800

    Adapt to openai

--- ai/agents/oai/zhipuaiadpter.py
+++ ai/agents/oai/zhipuaiadpter.py
@@ -8,6 +8,19 @@ from zhipuai import ZhipuAI
 ZHIPU_AI_MODEL = "glm-4"
 
 
+def object_to_dict(obj):
+    if isinstance(obj, (int, float, str, bool, type(None))):
+        return obj
+    elif isinstance(obj, list):
+        return [object_to_dict(item) for item in obj]
+    elif isinstance(obj, dict):
+        return {key: object_to_dict(value) for key, value in obj.items()}
+    elif hasattr(obj, "__dict__"):
+        return {key: object_to_dict(value) for key, value in obj.__dict__.items() if not key.startswith("__")}
+    else:
+        return str(obj)
+
+
 class ZhiPuAIClient:
 
     @classmethod
@@ -15,10 +28,12 @@ class ZhiPuAIClient:
         zhipu_data = cls.input_to_openai(data)
         client = ZhipuAI(api_key=apiKey)
         if "functions" in data:
+            tools = zhipu_data['tools']
             response = client.chat.completions.create(
                 model=ZHIPU_AI_MODEL,  # 填写需要调用的模型名称
                 messages=zhipu_data['messages'],
-                functions=zhipu_data["functions"]
+                tools=tools,
+                tool_choice="auto",
             )
         else:
             response = client.chat.completions.create(
@@ -30,10 +45,19 @@ class ZhiPuAIClient:
 
     @classmethod
     def input_to_openai(cls, data):
+        tools = []
+        if "functions" in data:
+            for item in data['functions']:
+                new_item = {
+                    "type": "function",
+                    "function": item
+                }
+                tools.append(new_item)
+        data['tools'] = tools
         return data
         pass
 
     @classmethod
     def output_to_openai(cls, data):
-        return data
+        return object_to_dict(data)
         pass

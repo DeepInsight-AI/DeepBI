7544b34f264ae2b4f4e581ee7b86c9a6849a6b34
zhuxiangchneg <1264521359@qq.com>
2024-01-30 17:22:38 +0800

    update log model

--- client/app/pages/testdialogue/components/LogWorkflow/index.jsx
+++ client/app/pages/testdialogue/components/LogWorkflow/index.jsx
@@ -3,6 +3,7 @@ import Loading from "../loading";
 import Button from "antd/lib/button";
 import CopyOutlinedIcon from "@ant-design/icons/CopyOutlined";
 import CompressOutlinedIcon from "@ant-design/icons/CompressOutlined";
+import Modal from "antd/lib/modal";
 import { markdown } from "markdown";
 import HtmlContent from "@/components/chart/components/HtmlContent";
 import toast from "react-hot-toast";
@@ -11,6 +12,7 @@ import Tooltip from "@/components/Tooltip";
 
 const LogWorkflow = props => {
   const { logData, Cardloading } = props;
+  const [isModalVisible, setIsModalVisible] = React.useState(false);
   const [sourceList, setSourceList] = React.useState([]);
   // workTitle
   const [workTitle, setWorkTitle] = React.useState("Show work");
@@ -19,15 +21,15 @@ const LogWorkflow = props => {
     if (logData && logData.length > 0) {
       const newLogData = logData.slice(sourceList.length);
       const log_terminal = logTerminal.current;
-      // const reg = /\n/g;
-      // const isHas = newLogData.some(item => reg.test(item));
-      // if (isHas) {
-        // const newSource = newLogData.map(item => item.replace(/\n\n/g, "<br />"));
-        const newSource = newLogData.map(item => markdown.toHTML(item));
+      const reg = /\n/g;
+      const isHas = newLogData.some(item => reg.test(item));
+      if (isHas) {
+        const newSource = newLogData.map(item => item.replace(/\n\n/g, "<br />"));
+        // const newSource = newLogData.map(item => markdown.toHTML(item));
         setSourceList(prevSourceList => [...prevSourceList, ...newSource]);
-      // } else {
-        // setSourceList(prevSourceList => [...prevSourceList, ...newLogData]);
-      // }
+      } else {
+        setSourceList(prevSourceList => [...prevSourceList, ...newLogData]);
+      }
       setTimeout(() => {
         log_terminal.scrollTop = log_terminal.scrollHeight;
       }, 0);
@@ -60,6 +62,32 @@ const LogWorkflow = props => {
       toast.error(window.W_L.copy_failed);
     }
   };
+
+  const compress = () => {
+    setIsModalVisible(true);
+  };
+  
+  const showModel = () => {
+    return (
+      <Modal
+        title="Log"
+        visible={isModalVisible}
+        onCancel={() => setIsModalVisible(false)}
+        footer={null}
+        closable={false}
+        width="80%"
+      >
+        <div className="log_terminal_content" ref={logTerminal}>
+        {sourceList &&
+            sourceList.length > 0 &&
+            sourceList.map((item, index) => {
+              return <div key={index} className="log-item" dangerouslySetInnerHTML={{ __html: item }}></div>;
+            })}
+        </div>
+      </Modal>
+    );
+  };
+
   return (
     <div className={`log_terminal`}>
       <div className="log_terminal_header">
@@ -75,29 +103,40 @@ const LogWorkflow = props => {
       </div>
       <div className="log_terminal_content_div">
         <Tooltip title={window.W_L.copy}>
-          <Button icon={<CopyOutlinedIcon />} style={{position: "absolute",color: "#9d9d9d"}} type="text" size="small" className="copy_btn" onClick={copy}>
+          <Button
+            icon={<CopyOutlinedIcon />}
+            style={{ position: "absolute", color: "#9d9d9d" }}
+            type="text"
+            size="small"
+            className="copy_btn"
+            onClick={copy}>
             {/* {window.W_L.copy} */}
           </Button>
         </Tooltip>
-        <Tooltip title={window.W_L.copy}>
-          <Button icon={<CompressOutlinedIcon />} style={{position: "absolute",color: "#9d9d9d"}} type="text" size="small" className="compress_btn" onClick={copy}>
+        <Tooltip title="全屏">
+          <Button
+            icon={<CompressOutlinedIcon />}
+            style={{ position: "absolute", color: "#9d9d9d" }}
+            type="text"
+            size="small"
+            className="compress_btn"
+            onClick={compress}>
             {/* {window.W_L.copy} */}
           </Button>
         </Tooltip>
+        {showModel()}
         <div className="log_terminal_content" ref={logTerminal}>
-          
-          {/* {sourceList &&
+          {sourceList &&
             sourceList.length > 0 &&
             sourceList.map((item, index) => {
               return <div key={index} className="log-item" dangerouslySetInnerHTML={{ __html: item }}></div>;
-            })} */}
-            
+            })}
 
-            {sourceList &&
+          {/* {sourceList &&
             sourceList.length > 0 &&
             sourceList.map((item, index) => {
               return <HtmlContent key={index} className="log-item preview markdown">{item}</HtmlContent>;
-            })}
+            })} */}
         </div>
       </div>
     </div>

5c10a51fbc3a2d39364932ca16467eb5b1abd3a2
zhuxiangchneg <1264521359@qq.com>
2024-02-04 11:01:25 +0800

    测试login

--- bi/handlers/feishu.py
+++ bi/handlers/feishu.py
@@ -3,39 +3,135 @@ import os
 import time
 import hashlib
 import requests
-from flask import request, jsonify
+from flask import request, jsonify,redirect,render_template
 from bi.handlers import routes  # 假设routes是全局可用的，根据你的项目结构导入
 from bi.handlers.feishu_auth import Auth  # 确保这个路径正确
 from dotenv import load_dotenv, find_dotenv
 import logging
 from bi.handlers.base import json_response, org_scoped_rule
+from bi import __version__, limiter, models, settings, __DeepBI_version__
+from bi.authentication import current_org, get_login_url, get_next_path
 # 加载环境变量
 load_dotenv(find_dotenv())
 
 logger = logging.getLogger(__name__)
-
+USER_INFO_KEY = "UserInfo"
+auth = null
 # @routes.route("/login", methods=["GET"])
+# @routes.route(org_scoped_rule("/login"), methods=["GET"])
+# def login():
+#     NONCE_STR = "13oEviLbrTo458A3NjrOwS70oTOXVOAm"
+#     APP_ID = os.getenv("APP_ID")
+#     APP_SECRET = os.getenv("APP_SECRET")
+#     FEISHU_HOST = os.getenv("FEISHU_HOST")
+#     auth = Auth(FEISHU_HOST, APP_ID, APP_SECRET)
+
+#     url = request.args.get("url")
+#     ticket = auth.get_ticket()
+#     timestamp = int(time.time()) * 1000
+#     verify_str = "jsapi_ticket={}&noncestr={}&timestamp={}&url={}".format(
+#         ticket, NONCE_STR, timestamp, url
+#     )
+#     signature = hashlib.sha1(verify_str.encode("utf-8")).hexdigest()
+
+#     return jsonify({
+#         "appid": APP_ID,
+#         "signature": signature,
+#         "noncestr": NONCE_STR,
+#         "timestamp": timestamp,
+#     })
+
+# 业务逻辑类
+class Biz(object):
+    @staticmethod
+    def home_handler():
+        # 主页加载流程
+        return Biz._show_user_info()
+
+    @staticmethod
+    def login_handler():
+        # 需要走免登流程
+        return render_template("login.html", user_info={"name": "unknown"}, login_info="needLogin")
+
+    @staticmethod
+    def login_failed_handler(err_info):
+        # 出错后的页面加载流程
+        return Biz._show_err_info(err_info)
+
+    # Session in Flask has a concept very similar to that of a cookie, 
+    # i.e. data containing identifier to recognize the computer on the network, 
+    # except the fact that session data is stored in a server.
+    @staticmethod
+    def _show_user_info():
+        # 直接展示session中存储的用户信息
+        return render_template("login.html", user_info=session[USER_INFO_KEY], login_info="alreadyLogin")
+
+    # @staticmethod
+    # def _show_err_info(err_info):
+        ## 将错误信息展示在页面上
+        # return render_template("err_info.html", err_info=err_info)
+
+# 出错时走错误页面加载流程Biz.login_failed_handler(err_info)
+@app.errorhandler(Exception)
+def auth_error_handler(ex):
+    return Biz.login_failed_handler(ex)
+
+
+# 默认的主页路径
+# @app.route("/login", methods=["GET"])
 @routes.route(org_scoped_rule("/login"), methods=["GET"])
-def login():
-    NONCE_STR = "13oEviLbrTo458A3NjrOwS70oTOXVOAm"
-    APP_ID = os.getenv("APP_ID")
-    APP_SECRET = os.getenv("APP_SECRET")
-    FEISHU_HOST = os.getenv("FEISHU_HOST")
-    auth = Auth(FEISHU_HOST, APP_ID, APP_SECRET)
+# @limiter.limit(settings.THROTTLE_LOGIN_PATTERN)
+def login(org_slug=None):
+    # APP_ID = os.getenv("APP_ID")
+    # APP_SECRET = os.getenv("APP_SECRET")
+    # FEISHU_HOST = os.getenv("FEISHU_HOST")
+    # auth = Auth(FEISHU_HOST, APP_ID, APP_SECRET)
+    auth = Auth(os.getenv("FEISHU_HOST"), os.getenv("APP_ID"), os.getenv("APP_SECRET"))
+    print("zxctest=====================")
+    # 打开本网页应用会执行的第一个函数
 
-    url = request.args.get("url")
-    ticket = auth.get_ticket()
-    timestamp = int(time.time()) * 1000
-    verify_str = "jsapi_ticket={}&noncestr={}&timestamp={}&url={}".format(
-        ticket, NONCE_STR, timestamp, url
+    index_url = url_for("bi.index", org_slug=org_slug)
+    unsafe_next_path = request.args.get("next", index_url)
+    next_path = get_next_path(unsafe_next_path)
+    if current_user.is_authenticated:
+        return redirect(next_path)
+
+    # 如果session当中没有存储user info，则走免登业务流程Biz.login_handler()
+    if USER_INFO_KEY not in session:
+        logging.info("need to get user information")
+        return Biz.login_handler()
+    else:
+        # 如果session中已经有user info，则直接走主页加载流程Biz.home_handler()
+        logging.info("already have user information")
+        return Biz.home_handler()
+
+@app.route("/callback", methods=["GET"])
+def callback():
+    # 获取 user info
+
+    # 拿到前端传来的临时授权码 Code
+    code = request.args.get("code")
+    # 先获取 user_access_token
+    auth.authorize_user_access_token(code)
+    # 再获取 user info
+    user_info = auth.get_user_info()
+    # 将 user info 存入 session
+    session[USER_INFO_KEY] = user_info
+    return jsonify(user_info)
+
+@app.route("/get_appid", methods=["GET"])
+def get_appid():
+    # 获取 appid
+    # 为了安全，app_id不应对外泄露，尤其不应在前端明文书写，因此此处从服务端传递过去
+    return jsonify(
+        {
+            "appid": os.getenv("APP_ID")
+        }
     )
-    signature = hashlib.sha1(verify_str.encode("utf-8")).hexdigest()
 
-    return jsonify({
-        "appid": APP_ID,
-        "signature": signature,
-        "noncestr": NONCE_STR,
-        "timestamp": timestamp,
-    })
-
-# 根据需要添加更多的路由和处理函数
+# 登录完成
+@app.route("/login_success", methods=["GET"])
+def login_success():
+    # 登录完成后，展示主页
+    print("login_success+++++++++++++++++++++") 
+    return jsonify({"msg": "login success-------------------"})
--- bi/handlers/feishu_auth.py
+++ bi/handlers/feishu_auth.py
@@ -1,10 +1,12 @@
 import requests
 import logging
+from urllib import parse
 
 # const
-# 开放接口 URI
-TENANT_ACCESS_TOKEN_URI = "/open-apis/auth/v3/tenant_access_token/internal"
-JSAPI_TICKET_URI = "/open-apis/jssdk/ticket/get"
+# open api capability
+USER_ACCESS_TOKEN_URI = "/open-apis/authen/v1/access_token"
+APP_ACCESS_TOKEN_URI = "/open-apis/auth/v3/app_access_token/internal"
+USER_INFO_URI = "/open-apis/authen/v1/user_info"
 
 
 class Auth(object):
@@ -12,31 +14,70 @@ class Auth(object):
         self.feishu_host = feishu_host
         self.app_id = app_id
         self.app_secret = app_secret
-        self.tenant_access_token = ""
+        self._app_access_token = ""
+        self._user_access_token = ""
 
-    def get_ticket(self):
-        # 获取jsapi_ticket，具体参考文档：https://open.feishu.cn/document/ukTMukTMukTM/uYTM5UjL2ETO14iNxkTN/h5_js_sdk/authorization
-        self.authorize_tenant_access_token()
-        url = "{}{}".format(self.feishu_host, JSAPI_TICKET_URI)
+    @property
+    def user_access_token(self):
+        return self._user_access_token
+
+    @property
+    def app_access_token(self):
+        return self._app_access_token
+
+    # 这里也可以拿到user_info
+    # 但是考虑到服务端许多API需要user_access_token，如文档：https://open.feishu.cn/document/ukTMukTMukTM/uUDN04SN0QjL1QDN/document-docx/docx-overview
+    # 建议的最佳实践为先获取user_access_token，再获得user_info
+    # user_access_token后续刷新可以参阅文档：https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/authen-v1/authen/refresh_access_token
+    def authorize_user_access_token(self, code):
+        # 获取 user_access_token, 依托于飞书开放能力实现. 
+        # 文档链接: https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/authen-v1/authen/access_token
+        self.authorize_app_access_token()
+        url = self._gen_url(USER_ACCESS_TOKEN_URI)
+        # “app_access_token” 位于HTTP请求的请求头
         headers = {
-            "Authorization": "Bearer " + self.tenant_access_token,
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + self.app_access_token,
+        }
+        # 临时授权码 code 位于HTTP请求的请求体
+        req_body = {"grant_type": "authorization_code", "code": code}
+        response = requests.post(url=url, headers=headers, json=req_body)
+        Auth._check_error_response(response)
+        self._user_access_token = response.json().get("data").get("access_token")
+
+    def get_user_info(self):
+        # 获取 user info, 依托于飞书开放能力实现.  
+        # 文档链接: https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/authen-v1/authen/user_info
+        url = self._gen_url(USER_INFO_URI)
+        # “user_access_token” 位于HTTP请求的请求头
+        headers = {
+            "Authorization": "Bearer " + self.user_access_token,
             "Content-Type": "application/json",
         }
-        resp = requests.post(url=url, headers=headers)
-        Auth._check_error_response(resp)
-        return resp.json().get("data").get("ticket", "")
+        response = requests.get(url=url, headers=headers)
+        Auth._check_error_response(response)
+        # 如需了解响应体字段说明与示例，请查询开放平台文档： 
+        # https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/authen-v1/authen/access_token
+        return response.json().get("data")
 
-    def authorize_tenant_access_token(self):
-        # 获取tenant_access_token，基于开放平台能力实现，具体参考文档：https://open.feishu.cn/document/ukTMukTMukTM/ukDNz4SO0MjL5QzM/auth-v3/auth/tenant_access_token_internal
-        url = "{}{}".format(self.feishu_host, TENANT_ACCESS_TOKEN_URI)
+    def authorize_app_access_token(self):
+        # 获取 app_access_token, 依托于飞书开放能力实现. 
+        # 文档链接: https://open.feishu.cn/document/ukTMukTMukTM/ukDNz4SO0MjL5QzM/auth-v3/auth/app_access_token_internal
+        url = self._gen_url(APP_ACCESS_TOKEN_URI)
+        # "app_id" 和 "app_secret" 位于HTTP请求的请求体
         req_body = {"app_id": self.app_id, "app_secret": self.app_secret}
         response = requests.post(url, req_body)
         Auth._check_error_response(response)
-        self.tenant_access_token = response.json().get("tenant_access_token")
+        self._app_access_token = response.json().get("app_access_token")
+
+    def _gen_url(self, uri):
+        # 拼接飞书开放平台域名feishu_host和uri
+        return "{}{}".format(self.feishu_host, uri)
 
     @staticmethod
     def _check_error_response(resp):
         # 检查响应体是否包含错误信息
+        # check if the response contains error information
         if resp.status_code != 200:
             raise resp.raise_for_status()
         response_dict = resp.json()
@@ -44,7 +85,6 @@ class Auth(object):
         if code != 0:
             logging.error(response_dict)
             raise FeishuException(code=code, msg=response_dict.get("msg"))
-
 
 class FeishuException(Exception):
     # 处理并展示飞书侧返回的错误码和错误信息

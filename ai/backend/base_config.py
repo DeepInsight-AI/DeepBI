1d4cd93e9ce7f6dfdd8be837ef6c53bf335a0238
lcx <825504994@qq.com>
2024-02-04 21:18:49 +0800

    update max_token_num / request_timeout

--- ai/backend/base_config.py
+++ ai/backend/base_config.py
@@ -10,7 +10,7 @@ class Config():
 
         # self.database_model = 'test'
         self.database_model = 'online'
-        self.request_timeout = 55
+        self.request_timeout = 90
         self.max_retry_period = 90
         self.max_retry_times = 3
         self.up_file_path = base_util.get_upload_path()
@@ -29,7 +29,7 @@ class Config():
 
         self.python_base_dependency = """python installed dependency environment: pymysql, pandas, mysql-connector-python, pyecharts, sklearn, psycopg2, sqlalchemy，pymongo"""
 
-        self.max_token_num = 6000
+        self.max_token_num = 7000
 
         self.talker_bi = 'bi'
         self.talker_user = 'user'

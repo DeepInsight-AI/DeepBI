e873589194e784209c5853d5c50c712cadb87dd3
lcx <825504994@qq.com>
2024-02-22 17:36:17 +0800

    fix AzureApiKey BUG

--- ai/backend/base_config.py
+++ ai/backend/base_config.py
@@ -68,4 +68,4 @@ class Config():
 
 
 CONFIG = Config()
-agents_functions=['task_generate_echart', 'task_generate_report', 'task_base']
+agents_functions = ['task_generate_echart', 'task_generate_report', 'task_base']

50f3e558fd678e48bec3352e5320ac7b61ed8e04
zhuxiangchneg <1264521359@qq.com>
2024-02-28 20:07:57 +0800

    test feishu login multi

--- bi/authentication/__init__.py
+++ bi/authentication/__init__.py
@@ -249,6 +249,14 @@ def redirect_to_login():
         response.status_code = 404
         return response
     print("redirect_to_login+++++++",request.url)
+    # 
+    next = request.url
+    if "UserInfo" in session:
+        print("session",session["UserInfo"])
+        open_id = session["UserInfo"]["open_id"]
+        if "default" in next:
+            next = next.replace("default",open_id)
+    print("redirect_login+++++++",request.url)
     login_url = get_login_url(next=request.url, external=False)
 
     return redirect(login_url)

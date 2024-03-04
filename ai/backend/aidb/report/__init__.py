44587333f2327ff4447586c0d6f038cc1dc19d74
BITFlyPig <161425767+BITFlyPig@users.noreply.github.com>
2024-03-04 14:28:03 +0800

    Add files via upload

--- ai/backend/aidb/report/__init__.py
+++ ai/backend/aidb/report/__init__.py
@@ -2,3 +2,4 @@ from .report_mysql import ReportMysql
 from .report_pg import ReportPostgresql
 from .report_sr import ReportStarrocks
 from .report_mongodb import ReportMongoDB
+from .report_sqlserver import ReportSqlserver

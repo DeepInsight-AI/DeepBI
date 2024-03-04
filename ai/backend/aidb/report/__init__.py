2e9f5d4980420cd4d8763cee0ecc11271e0aa7d2
BITFlyPig <161425767+BITFlyPig@users.noreply.github.com>
2024-03-04 14:45:59 +0800

    Update __init__.py

--- ai/backend/aidb/report/__init__.py
+++ ai/backend/aidb/report/__init__.py
@@ -3,4 +3,3 @@ from .report_pg import ReportPostgresql
 from .report_sr import ReportStarrocks
 from .report_mongodb import ReportMongoDB
 from .report_sqlserver import ReportSqlserver
-from ai.backend.aidb.autopilot AnalysisSqlserver

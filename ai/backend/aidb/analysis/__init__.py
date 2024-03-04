786a1e61919325460790f70d00a560479f712889
BITFlyPig <161425767+BITFlyPig@users.noreply.github.com>
2024-03-04 14:25:53 +0800

    Add files via upload

--- ai/backend/aidb/analysis/__init__.py
+++ ai/backend/aidb/analysis/__init__.py
@@ -3,3 +3,4 @@ from .analysis_csv import AnalysisCsv
 from .analysis_pg import AnalysisPostgresql
 from .analysis_sr import AnalysisStarrocks
 from .analysis_mongodb import AnalysisMongoDB
+from .analysis_sqlserver import AnalysisSqlserver

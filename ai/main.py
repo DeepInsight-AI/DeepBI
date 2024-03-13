344d77f6c111087def0e9e9b4c4a854da3c0b981
zhuxiangchneg <1264521359@qq.com>
2024-03-13 16:57:18 +0800

    update cors

--- ai/main.py
+++ ai/main.py
@@ -3,8 +3,9 @@ import asyncio
 from ai.backend.chat_task import ChatClass
 from ai.backend.aidb.autopilot.autopilot_mysql_api import AutopilotMysql
 from concurrent.futures import ThreadPoolExecutor
-
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/api/chat", methods=["POST"])
 def chat():

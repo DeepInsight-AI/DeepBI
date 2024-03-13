7f8327ee919e055568229d1afe73c1234223cf56
zhuxiangchneg <1264521359@qq.com>
2024-03-13 17:01:16 +0800

    update cors

--- ai/main.py
+++ ai/main.py
@@ -3,11 +3,11 @@ import asyncio
 from ai.backend.chat_task import ChatClass
 from ai.backend.aidb.autopilot.autopilot_mysql_api import AutopilotMysql
 from concurrent.futures import ThreadPoolExecutor
-from flask_cors import CORS
+from flask_cors import cross_origin
 app = Flask(__name__)
-CORS(app)
 
 @app.route("/api/chat", methods=["POST"])
+@cross_origin()  # 允许所有来源
 def chat():
     data = request.get_json()
     print("data: ", data)

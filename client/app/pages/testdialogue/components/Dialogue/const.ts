125b540ed2b63a37de991a4efeab7a8222c900b4
zhuxiangchneg <1264521359@qq.com>
2024-03-13 16:41:16 +0800

    update const

--- client/app/pages/testdialogue/components/Dialogue/const.ts
+++ client/app/pages/testdialogue/components/Dialogue/const.ts
@@ -1,4 +1,6 @@
 
 export const DEEPBI_API_HOST = process.env.SOCKET
 
-export const API_CHAT = `${DEEPBI_API_HOST}/api/chat` as const;
+export const API_LOCAL = window.location.protocol + '//'
+
+export const API_CHAT = `${API_LOCAL}${DEEPBI_API_HOST}/api/chat` as const;

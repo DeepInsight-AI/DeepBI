7d3b3ee60791d5ea9fbf06315f6d5d836490247c
zhuxiangchneg <1264521359@qq.com>
2024-03-18 19:13:50 +0800

    update useChartCode

--- client/app/pages/testdialogue/components/Dialogue/generateChart.js
+++ client/app/pages/testdialogue/components/Dialogue/generateChart.js
@@ -12,17 +12,14 @@ export function generateChart(
     wsProtocol = 'wss';
   }
   const wsUrl = `${wsProtocol}://${process.env.SOCKET}/${currentUser.id}_${currentUser.name}`;
-  console.log("Connecting to backend @ ", wsUrl);
 
-  let ws = null;
   if (wsRef.current && wsRef.current.readyState === 1){
     console.log("old ws connection is open")
-    ws = wsRef.current;
-    console.log("old params", params)
+    // console.log("old params", params)
     wsRef.current.send(JSON.stringify(params));
   }else{
     console.log("new ws connection")
-    ws = new WebSocket(wsUrl);
+    const ws = new WebSocket(wsUrl);
     wsRef.current = ws;
     ws.addEventListener("open", () => {
       console.log("new params", params);
@@ -32,17 +29,17 @@ export function generateChart(
 
   
 
-  ws.addEventListener("message", async (event) => {
+  wsRef.current.addEventListener("message", async (event) => {
     const response = JSON.parse(event.data);
     onChange(response);
   });
 
-  ws.addEventListener("close", (event) => {
+  wsRef.current.addEventListener("close", (event) => {
     console.log("Connection closed", event.code, event.reason);
     onCancel();
   });
 
-  ws.addEventListener("error", (error) => {
+  wsRef.current.addEventListener("error", (error) => {
     console.error("WebSocket error", error);
     toast.error(window.W_L.ERROR_MESSAGE);
   });

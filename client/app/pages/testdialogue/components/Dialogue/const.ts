5c60a5495364e811b6c15e6a577057088800d106
zhuxiangchneg <1264521359@qq.com>
2024-03-26 18:54:14 +0800

    update const

--- client/app/pages/testdialogue/components/Dialogue/const.ts
+++ client/app/pages/testdialogue/components/Dialogue/const.ts
@@ -1,6 +1,8 @@
 
-export const DEEPBI_API_HOST = process.env.SSE_URL
+// export const DEEPBI_API_HOST = process.env.SSE_URL
 
-export const API_LOCAL = window.location.protocol + '//'
+// export const API_LOCAL = window.location.protocol + '//'
 
-export const API_CHAT = `${API_LOCAL}${DEEPBI_API_HOST}/api/chat` as const;
+// export const API_CLOAL_HOST = `${API_LOCAL}${DEEPBI_API_HOST}` as const;
+
+export const API_CHAT = `api/chat` as const;

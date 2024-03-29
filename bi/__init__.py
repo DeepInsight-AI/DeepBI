5c4411dfc92641e1bb177bdcc60ef557a6fd8e49
wangshiyouyang <luhuijie@deep-thought.io>
2024-03-29 14:18:34 +0800

    update version info

--- bi/__init__.py
+++ bi/__init__.py
@@ -15,8 +15,8 @@ from .app import create_app  # noqa
 from .query_runner import import_query_runners
 from .destinations import import_destinations
 
-__version__ = "1.2.3"
-__DeepBI_version__ = "1.2.3"
+__version__ = "1.3.0"
+__DeepBI_version__ = "1.3.0"
 
 
 def setup_logging():

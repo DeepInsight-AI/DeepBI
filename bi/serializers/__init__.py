6e3ffa0fa2a31bd6b16df2959614a52b24420d69
zhuxiangchneg <1264521359@qq.com>
2024-04-10 15:46:12 +0800

    test DataSourceSchemaResource

--- bi/serializers/__init__.py
+++ bi/serializers/__init__.py
@@ -328,7 +328,7 @@ def serialize_job(job):
     else:
         error = ""
         result = query_result_id = job.result
-
+    print("result===", result)
     return {
         "job": {
             "id": job.id,

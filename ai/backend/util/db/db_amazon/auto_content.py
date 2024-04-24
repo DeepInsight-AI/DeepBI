d90a88f7ce46743670c9af0e46b6668181a02e42
auserwn <auserwn@163.com>
2024-04-24 15:54:26 +0800

    update auto_content

--- ai/backend/util/db/db_amazon/auto_content.py
+++ ai/backend/util/db/db_amazon/auto_content.py
@@ -22,3 +22,48 @@ REPORT_THOUGHT=[{'report_name': '现状分析', 'description': ''},
                                   ]
 REPORT_ANALYST=[{'analysis_item': '优化策略',
                                    'description': '综合以上检查报告，以下为推荐在广告后台执行的优化策略'}]
+
+
+REPORT_THOUGHT_SD=[{'report_name': '现状分析', 'description': ''},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '走势分析 - 过去2周，按日统计，每天汇总的 ACOS，点击量，转化率，CPC，广告消耗 '},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '均值分析 - 过去2周，平均的 ACOS，点击量，转化率，CPC，广告消耗'},
+                                  {'report_name': '统计维度解释', 'description': ''},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '转化率： 7天转化率'},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': 'ACOS：7天转化率产生的ACOS'},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '优质定义：低于平均 ACOS 10% 及以下'},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '劣质定义：高于平均 ACOS 10% 及以上'},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '平均ACOS计算： xx天 广告总消耗 / xx天 广告总销售额  * 100%'},
+                                  {'report_name': '提问常见问题补充', 'description': ''},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '1. ACOS算法和比较时需要检查SQL，字段出错需要备注'},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '2. 产品asin和sku联表需要检查是否提取了正确字段'},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '3. ACOS的倍数计算需要检查AI是否理解正确，考虑直接标出具体ACOS数据'},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '4. 购物意图部分标签相关主要字段为targeting text, targetingId '},
+                                  ]
+
+REPORT_THOUGHT_NEW_SB=[{'report_name': '现状分析', 'description': ''},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '走势分析 - 过去2周，按日统计，每天汇总的 ACOS，点击量，转化率，CPC，广告消耗 '},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '均值分析 - 过去2周，平均的 ACOS，点击量，转化率，CPC，广告消耗'},
+                                  {'report_name': '统计维度解释', 'description': ''},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '转化率： 7天转化率'},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': 'ACOS：7天转化率产生的ACOS'},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '优质定义：低于平均 ACOS 10% 及以下'},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '劣质定义：高于平均 ACOS 10% 及以上'},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '平均ACOS计算： xx天 广告总消耗 / xx天 广告总销售额  * 100%'},
+                                  {'report_name': '关键词优化分析', 'description': ''},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '1，添加未在投放中的 优质 用户搜索关键词到 定向投放关键词计划中'},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '2，找出 定向投放关键词 中的 优质关键词，提价操作'},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '3，同类型产品广告计划中，相关的优秀关键词应该全覆盖'},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '4，找出 定向投放关键词 中的 劣质关键词'},
+                                  {'report_name': '商品优化分析', 'description': ''},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;','description': '1，找出 sku广告 中的 优质广告'},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;','description': '2，找出 sku广告 中的 劣质广告'},
+                                  {'report_name': '广告计划优化分析', 'description': ''},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '1，找出 campaign 广告活动 中的  优质 与 劣质广告活动'},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '2，找出 campaign 广告活动 中的  placement优质位置 与 劣质位置'},
+                                  {'report_name': '&nbsp;&nbsp;&nbsp;&nbsp;', 'description': '3，找出 广告 adgroup 中的  优质 与 劣质广告组'},
+                       {'report_name': '新开SB广告整体策略', 'description': '找出别国SB广告，以及意大利SP广告的优质关键词，汇总后作为意大利新开SB广告的优质关键词'}
+                                  ]
+
+
+REPORT_ANALYST_new_SB=[{'analysis_item': '优化策略',
+                                   'description': '将各国的SB广告的优质关键词汇总，作为意大利新开SB广告的优质关键词\n把意大利SP广告的优质关键词作为意大利新开SB广告的优质关键词'}]

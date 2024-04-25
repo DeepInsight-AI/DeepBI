6208f9f38bd36ac4cddccb5206e7461a7852b7fb
zhuxiangchneg <1264521359@qq.com>
2024-04-25 12:05:32 +0800

    update DEEPBI_API_HOST

--- client/app/pages/testdialogue/components/Dialogue/const.ts
+++ client/app/pages/testdialogue/components/Dialogue/const.ts
@@ -1,4 +1,4 @@
-export const DEEPBI_API_HOST = process.env.SSE_URL || (window.location.protocol + '//' + window.location.hostname + ':8341');
+export const DEEPBI_API_HOST = window.location.protocol + '//' + process.env.SSE_URL || ( window.location.hostname + ':8341');
 
 // export const API_CLOAL_HOST = `${API_LOCAL}${DEEPBI_API_HOST}` as const;
 

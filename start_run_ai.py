2522dde1c43e13a6a226a67ae96b407286cb1d8b
lcx <825504994@qq.com>
2024-05-07 16:27:22 +0800

    update start_run_ai

--- start_run_ai.py
+++ start_run_ai.py
@@ -1,15 +1,21 @@
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
+#
+# # 加载 .env 文件中的环境变量
+# load_dotenv()
+#
+# # import asyncio
+# from ai.backend.start_server import WSServer
+#
+# if __name__ == '__main__':
+#     server_port = 8339
+#     s = WSServer(server_port)
+#     # t = threading.Thread(target=s.serve_forever)
+#     # t.daemon = True
+#     # t.start()
+#     s.serve_forever()
 
-# 加载 .env 文件中的环境变量
-load_dotenv()
 
-import asyncio
-from ai.backend.start_server import WSServer
-
+import ai_chat_api
 if __name__ == '__main__':
-    server_port = 8339
-    s = WSServer(server_port)
-    # t = threading.Thread(target=s.serve_forever)
-    # t.daemon = True
-    # t.start()
-    s.serve_forever()
+    app = ai_chat_api.app
+    app.run(port=8341, host='0.0.0.0')
